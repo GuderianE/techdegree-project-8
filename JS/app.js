1390b16b36931250a10a7a3b7b909b416025040f
Elias Guderian <46761652+GuderianE@users.noreply.github.com>
2019-08-14 23:37:46 +0200

    app works now changed fetch request

--- JS/app.js
+++ JS/app.js
@@ -3,25 +3,20 @@ const randomUserUrl = 'https://randomuser.me/api/?results=12';
 // const employee = document.querySelector('.employee');
 const userList = document.querySelector('#user-list');
 
-const fetchRequest = async (url) => {
-    try {
-    const fetchUrl = await fetch(url);
-    const response = await fetchUrl.json();
-    const jsonObject = await Object.values(response.results);
-    return await jsonObject;
-    } catch(error) {
-        throw error;
-    }
-};
-console.log(fetchRequest(randomUserUrl));
+fetch(randomUserUrl)
+    .then(res => res.json())
+    .then(data => createHTML(data))
+    .catch(err => console.log(err));
+
+
 function createHTML (data) {
     userData = data;
-    const liElement = document.createElement('li');
-    const promises = userData.results.forEach(async data => {
-        userList.classList.appendChild(liElement);
+    const promises = userData.results.forEach( data => {
+        const liElement = document.createElement('li');
+        userList.appendChild(liElement);
         liElement.innerHTML = `
           <div class="wrapper">  
-          <img class="profileImage" src="${data.picture.thumbnail}">
+          <img class="profileImage" src="${data.picture.large}">
           <div class="userData">
           <h2>${data.name.first} ${data.name.last}</h2>
           <p>${data.email}</p>
@@ -29,13 +24,9 @@ function createHTML (data) {
           </div>
           </div>
         `;
-        li = liElement.innerHTML;
+        li = liElement;
         li.className = "employee";
     });
     return Promises.all(promises);
 }
-
-const randomUser = fetchRequest(randomUserUrl);
-
-createHTML(randomUser);
 
